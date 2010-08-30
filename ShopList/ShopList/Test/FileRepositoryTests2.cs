05126b45be51b1111e2f33bcf68ec2a24623520a
Andreas Böckert <andreas.bockert@gmail.com>
2010-08-30 09:34:19 +0100

    Did stuff.

--- ShopList/ShopList/Test/FileRepositoryTests2.cs
+++ ShopList/ShopList/Test/FileRepositoryTests2.cs
@@ -1,51 +1,44 @@
+// ReSharper disable InconsistentNaming
 using System;
 using Machine.Specifications;
 using Machine.Specifications.DevelopWithPassion.Rhino;
 using ShopList.Model;
-using Rhino.Mocks;
 
 namespace ShopList.Test
 {
-    public class when_saving_to_repository : Observes<FileShoppingListRepository>
+    public class FileRepositoryTests2
     {
-        Establish c = () =>
-            {
-                _id = Guid.NewGuid();
-                _list = an<IShoppingList>();
-                _list.Id = _id;
-            };
+        public class concern : Observes<FileShoppingListRepository>
+        {
+            Establish c = () =>
+                {
+                    _list = an<IShoppingList>();
+                    _id = Guid.NewGuid();
+                    _list.Id = _id;
+                    _formattedListId = _list.Id.ToString("N").ToUpper();
+                };
 
-        Because b = () =>
-            {
-                sut.Add(_list);
-            };
+            public static IShoppingList _list;
+            public static string _formattedListId;
+            public static Guid _id;
+        }
 
-        It should_use_the_list_id_to_form_the_file_name = () =>
-            {
-                the_dependency<IFileSystem>().received(fs => fs.Save(_id.ToString("N").ToUpper(), null));
-            };
+        public class when_saving_to_repository : concern
+        {
+            Because b = () => sut.Add(_list);
 
-        static IShoppingList _list;
-        static Guid _id;
-    }
-   public class when_loading_from_repository : Observes<FileShoppingListRepository>
-    {
-        Establish c = () =>
-            {
-                _id = Guid.NewGuid();
-            };
+            It should_use_the_list_id_to_form_the_file_name = () =>
+                the_dependency<IFileSystem>().received(fs => fs.Save(_formattedListId, null));
+        }
 
-        Because b = () =>
-            {
-                _result = sut.Get(_id);
-            };
+        public class when_loading_from_repository : concern
+        {
+            Because b = () => { _result = sut.Get(_id); };
 
-        It should_use_the_list_id_to_form_the_file_name = () =>
-            {
-                the_dependency<IFileSystem>().received(fs => fs.Load(_id.ToString("N").ToUpper()));
-            };
+            It should_use_the_id_to_form_the_file_name = () =>
+                the_dependency<IFileSystem>().received(fs => fs.Load(_formattedListId));
 
-        static Guid _id;
-       static IShoppingList _result;
+            static IShoppingList _result;
+        }
     }
 }
